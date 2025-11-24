4994094ed75b1b204430419202a80548f3016272
Luciano <barberisluciano58@gmail.com>
2025-11-24 17:36:56 -0300

    Primer Commit

---
+++ src/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace RussoPriottiBarberis_GestorAlumnos
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
