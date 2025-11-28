b2afbdc2f8643e9e96e3ce66fa4fc6096e369b5d
NachiRusso <nachiiac06@gmail.com>
2025-11-28 18:02:42 -0300

    piolon ++

--- src/GestorArchivos.cs
+++ src/GestorArchivos.cs
@@ -197,6 +197,7 @@ namespace RussoPriottiBarberis_GestorAlumnos
                     }
                 }
 
+
                 Console.WriteLine("==============================================================");
                 Console.WriteLine("| Legajo | Apellido | Nombre | Nro. Doc. | Email | Teléfono |");
                 for (int i = 0; i < alumnoList.Count; i++)
@@ -227,8 +228,8 @@ namespace RussoPriottiBarberis_GestorAlumnos
                 }
                 alumnoList.RemoveAt(0); // Eliminamos el encabezado
                 Console.WriteLine("==============================================================");
-                Console.WriteLine("| Legajo | Apellido | Nombre | Nro. Doc. | Email | Teléfono |");
-                for (int i = 0; i < alumnoList.Count; i++)
+                Console.WriteLine("| Legajo | Apellido | Nombre | Nro. Doc. | Email | Telefono |");
+                for (int i = 1; i < alumnoList.Count; i++)
                 {
                     Console.WriteLine($"| {alumnoList[i].Legajo} | {alumnoList[i].Apellido} | {alumnoList[i].Nombre} | {alumnoList[i].Doc} | {alumnoList[i].Email} | {alumnoList[i].Tel}");
                     if ((i + 1) % 20 == 0)
@@ -282,9 +283,9 @@ namespace RussoPriottiBarberis_GestorAlumnos
             string fullPath = Path.Combine(FolderPath, fileName);
 
             // Cargar todos los registros en memoria
-            alumnosEnMemoria = Conversor.FromFileToObj(fullPath);
+            alumnosEnMemoria = Conversor.FromFileToObj(fileName);
 
-            if (formatoOriginal == string.Empty)
+            if (!EsFormatoValido(fileName))
             {
                 Console.WriteLine("\nPresione una tecla para volver al menú principal...");
                 Console.ReadKey();
